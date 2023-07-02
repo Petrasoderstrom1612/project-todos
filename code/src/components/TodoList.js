fd95b039e67ed86e03819d914e112712feaba3b4
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2023-07-02 23:16:01 +0200

    changed local storage to firebase

--- code/src/components/TodoList.js
+++ code/src/components/TodoList.js
@@ -1,27 +1,60 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect } from 'react';
-import styled from 'styled-components/macro'
+import styled from 'styled-components/macro';
 import { useSelector, useDispatch } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import todos from 'reducers/todos';
 
 const TodoList = () => {
   const dispatch = useDispatch();
+  const todoList = useSelector((store) => store.todos.items);
+
   useEffect(() => {
-    const listFromStorage = JSON.parse(localStorage.getItem('toDoList'))
-    if (listFromStorage) {
-      dispatch(todos.actions.setAllItems(listFromStorage))
-    }
-  }, [dispatch])
-  const todoList = useSelector((store) => store.todos.items)
+    const db = firebase.firestore();
+    const todosCollectionRef = db.collection('todos');
+
+    const fetchData = async () => {
+      const snapshot = await todosCollectionRef.get();
+      const todosData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data().name,
+        isDone: doc.data().isDone
+      }));
+      dispatch(todos.actions.setAllItems(todosData));
+    };
+
+    fetchData();
+  }, [dispatch]);
+
   const onIsDoneToggle = (id) => {
-    dispatch(todos.actions.toggleTodoItem(id))
-  }
-  const onDeleteTodoButtonClick = (todosIndex) => {
-    dispatch(todos.actions.deleteTodoItem(todosIndex))
-  }
+    dispatch(todos.actions.toggleTodoItem(id));
+  };
+
+  const onDeleteTodoButtonClick = async (todosIndex) => {
+    const db = firebase.firestore();
+    const todoToDelete = todoList[todosIndex];
+    await db.collection('todos').doc(todoToDelete.id).delete();
+    dispatch(todos.actions.deleteTodoItem(todosIndex));
+  };
+
   useEffect(() => {
-    localStorage.setItem('toDoList', JSON.stringify(todoList))
-  }, [todoList])
+    const db = firebase.firestore();
+    const todosCollectionRef = db.collection('todos');
+
+    const updateTodoList = async () => {
+      const batch = db.batch();
+      todoList.forEach((todo) => {
+        const docRef = todosCollectionRef.doc(todo.id);
+        batch.update(docRef, { isDone: todo.isDone });
+      });
+      await batch.commit();
+    };
+
+    updateTodoList();
+  }, [todoList]);
+
   return (
     <>
       {todoList.map((singleTodo, index) => {
@@ -32,16 +65,24 @@ const TodoList = () => {
               id="checkbox"
               checked={singleTodo.isDone}
               className="strikethrough"
-              onChange={() => onIsDoneToggle(singleTodo.id)} />
-            <label className="strikethrough" id="strikethrough">{singleTodo.name}
+              onChange={() => onIsDoneToggle(singleTodo.id)}
+            />
+            <label className="strikethrough" id="strikethrough">
+              {singleTodo.name}
             </label>
-            <RemoveButtonStyled onClick={() => onDeleteTodoButtonClick(index)} type="button" className="btn btn-warning">Remove</RemoveButtonStyled>
+            <RemoveButtonStyled
+              onClick={() => onDeleteTodoButtonClick(index)}
+              type="button"
+              className="btn btn-warning"
+            >
+              Remove
+            </RemoveButtonStyled>
           </EachTaskStyler>
         );
       })}
     </>
-  )
-}
+  );
+};
 
 export default TodoList;
 
